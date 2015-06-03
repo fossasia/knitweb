31e9676e08e8363900a58ada67118d8787f64888
sameera <sameera910409@gmail.com>
2015-06-03 15:43:02 +0530

    image pixelating script added

--- app/controller/editor.js
+++ app/controller/editor.js
@@ -1,6 +1,6 @@
-  var url;
+var url;
 
-   function readURL(input) {
+function readURL(input) {
 
     if (input.files && input.files[0]) {
         var reader = new FileReader();
@@ -8,51 +8,50 @@
 
         reader.onload = function (e) {
             $('#img_loader').attr('src', e.target.result)
-		    .width(350)
-                    .height(350);
+                .width(350)
+                .height(350);
         }
-
         reader.readAsDataURL(input.files[0]);
     }
 }
 
-  $("#file").change(function(){
+$("#file").change(function () {
     readURL(this);
-  });
- 
+});
 
-  function getURL(){
+
+function getURL() {
     return url;
-  }
+}
 
-  function file_select_handler(to_upload) {
-      var progressbar = $('#progressbar');
-      var status = $('#status');
-      var xhr = new XMLHttpRequest();
-      xhr.upload.addEventListener('loadstart', function(e1){
-          status.text('uploading image');
-          progressbar.progressbar({max: e1.total});
-      });
-      xhr.upload.addEventListener('progress', function(e1){
-          if (progressbar.progressbar('option', 'max') == 0)
-              progressbar.progressbar('option', 'max', e1.total);
-          progressbar.progressbar('value', e1.loaded);
-      });
-      xhr.onreadystatechange = function(e1) {
-          if (this.readyState == 4)  {
-              if (this.status == 200)
-                  var text = 'upload complete: ' + this.responseText;
-              else
-                  var text = 'upload failed: code ' + this.status;
-              status.html(text + '<br/>Select an image');
-              progressbar.progressbar('destroy');
-          }
-      };
-      xhr.open('POST', '/post', true);
-      xhr.send(to_upload);
-  };
+function file_select_handler(to_upload) {
+    var progressbar = $('#progressbar');
+    var status = $('#status');
+    var xhr = new XMLHttpRequest();
+    xhr.upload.addEventListener('loadstart', function (e1) {
+        status.text('uploading image');
+        progressbar.progressbar({max: e1.total});
+    });
+    xhr.upload.addEventListener('progress', function (e1) {
+        if (progressbar.progressbar('option', 'max') == 0)
+            progressbar.progressbar('option', 'max', e1.total);
+        progressbar.progressbar('value', e1.loaded);
+    });
+    xhr.onreadystatechange = function (e1) {
+        if (this.readyState == 4) {
+            if (this.status == 200)
+                var text = 'upload complete: ' + this.responseText;
+            else
+                var text = 'upload failed: code ' + this.status;
+            status.html(text + '<br/>Select an image');
+            progressbar.progressbar('destroy');
+        }
+    };
+    xhr.open('POST', '/post', true);
+    xhr.send(to_upload);
+};
 
-  $('#file').change(function(e){
-      file_select_handler(e.target.files[0]);
-      e.target.value = '';
-  });
+$('#file').change(function (e) {
+    file_select_handler(e.target.files[0]);
+    e.target.value = '';
+});
