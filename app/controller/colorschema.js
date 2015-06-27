516578bf780db4bf4f5727a4c056062608903aa8
sameera <sameera910409@gmail.com>
2015-06-27 17:01:48 +0530

    drawing tool functionality added

--- app/controller/colorschema.js
+++ app/controller/colorschema.js
@@ -5,21 +5,20 @@ var rArr = [];
 var rdArr = [];
 var gArr = [];
 var bArr = [];
-var imageDataArr =[];
+var imageDataArr = [];
 var imageArr = [];
+var list;
 
 var rowCount;
-var mouseX;
-var mouseY;
+var isFreeHand = false;
 
-window.onload = function(){
-    console.log("loaded");
-    for(var i=0;i<=100;i++){
-        imageDataArr[i]=[];
+window.onload = function () {
+    for (var i = 0; i <= 100; i++) {
+        imageDataArr[i] = [];
         imageArr[i] = []
-        for(var j=0;j<=100;j++){
-            imageDataArr[i][j]=false;
-            imageArr[i][j] =false;
+        for (var j = 0; j <= 100; j++) {
+            imageDataArr[i][j] = false;
+            imageArr[i][j] = false;
         }
     }
 }
@@ -54,9 +53,9 @@ $('#addBtn').colpick({
 })
 
 $('#picker1').colpick({
-    flat:true,
-    layout:'hex',
-    submit:0
+    flat: true,
+    layout: 'hex',
+    submit: 0
 });
 
 
@@ -70,142 +69,184 @@ $('.color-box').colpick({
     }
 })
 
-var rect,startX,startY,endX,endY;
-var startPixelX,startPixelY,endPixelX,endPixelY,pixelWidth,pixelHeight,prevPixelX,prevPixelY;
+var rect, startX, startY, endX, endY;
+var startPixelX, startPixelY, endPixelX, endPixelY, pixelWidth, pixelHeight, prevPixelX, prevPixelY;
 
 var pixelCountX = 0;
 var pixelCountY = 0;
+
+var temp;
 //getting canvas position for select tool
-layoutCanvas.onmousedown = function(e){
+layoutCanvas.onmousedown = function (e) {
     var mousemove = false;
     var count = 0;
+    list = [];
+    rect = pixelCanvas.getBoundingClientRect();
+    startX = e.clientX - rect.left;
+    startY = e.clientY - rect.top;
+    layoutCtx.clearRect(0, 0, 1000, 1000);
 
-     rect = pixelCanvas.getBoundingClientRect();
-     startX = e.clientX - rect.left;
-     startY = e.clientY - rect.top;
-     layoutCtx.clearRect(0,0,1000,1000);
-
-    prevPixelX = Math.floor(startX/10)*10;
-    prevPixelY = Math.floor(startY/10)*10;
-
-    document.onmousemove = function(e){
+    prevPixelX = Math.floor(startX / 10) * 10;
+    prevPixelY = Math.floor(startY / 10) * 10;
+    document.onmousemove = function (e) {
         mousemove = true;
 
         endX = e.clientX - rect.left;
         endY = e.clientY - rect.top;
-        startPixelX = Math.floor(startX/10)*10;
-        startPixelY = Math.floor(startY/10)*10;
-        endPixelX = Math.floor(endX/10)*10;
-        endPixelY = Math.floor(endY/10)*10;
+        startPixelX = Math.floor(startX / 10) * 10;
+        startPixelY = Math.floor(startY / 10) * 10;
+        endPixelX = Math.floor(endX / 10) * 10;
+        endPixelY = Math.floor(endY / 10) * 10;
 
         pixelWidth = startPixelX - endPixelX;
         pixelHeight = startPixelY - endPixelY;
 
-        if(pixelHeight<0)pixelHeight= -pixelHeight;
-        if(pixelWidth<0)pixelWidth = -pixelWidth;
+        if (pixelHeight < 0)pixelHeight = -pixelHeight;
+        if (pixelWidth < 0)pixelWidth = -pixelWidth;
 
-//        layoutCtx.clearRect(0,0,1000,1000);
-//        layoutCtx.strokeStyle = "rgba(255,0,0,255)";
-//        layoutCtx.lineWidth = 1;
-//        layoutCtx.strokeRect(startPixelX,startPixelY,pixelWidth,pixelHeight);
-
-        layoutCtx.beginPath();
-        layoutCtx.moveTo(prevPixelX,prevPixelY);
-        layoutCtx.lineTo(prevPixelX,prevPixelY);
-        imageDataArr[prevPixelX/10][prevPixelY/10] = true;
-
-        if(prevPixelX == endPixelX){
-           pixelCountX++;
-        }
-        if(prevPixelY == endPixelY){
-            pixelCountY++;
-        }
-
-        if(prevPixelX!=endPixelX && prevPixelY!=endPixelY){
-            layoutCtx.lineTo(endPixelX,prevPixelY);
-            imageDataArr[endPixelX/10][prevPixelY/10]=true;
-            if(prevPixelX!=endPixelX){
-                pixelCountY = 0;
-            }
-            if(prevPixelY!=endPixelY){
-                pixelCountX = 0;
-            }
-        }
-        layoutCtx.lineTo(endPixelX,endPixelY);
-        imageDataArr[endPixelX/10][endPixelY/10]=true;
-
-        layoutCtx.strokeStyle = "rgba(255,0,0,255)";
-        layoutCtx.stroke();
-        prevPixelX = endPixelX;
-        prevPixelY = endPixelY;
-
-    }
-
-    document.onmouseup = function() {
-        	    document.onmousemove = null
-        startPixelX = Math.floor(startX/10)*10;
-        startPixelY = Math.floor(startY/10)*10;
-        endPixelX = Math.floor(endX/10)*10;
-        endPixelY = Math.floor(endY/10)*10;
-
-        pixelWidth = startPixelX - endPixelX;
-        pixelHeight = startPixelY - endPixelY;
-
-        if(pixelHeight<0)pixelHeight= -pixelHeight;
-        if(pixelWidth<0)pixelWidth = -pixelWidth;
-
-        if(mousemove){
+        if (!isFreeHand) {
+            layoutCtx.clearRect(0, 0, 1000, 1000);
             layoutCtx.strokeStyle = "rgba(255,0,0,255)";
             layoutCtx.lineWidth = 1;
-            layoutCtx.strokeRect(startPixelX,startPixelY,pixelWidth,pixelHeight);
+            layoutCtx.strokeRect(startPixelX, startPixelY, pixelWidth, pixelHeight);
         }
-   //     pixelCtx.clearRect(startPixelX,startPixelY,pixelWidth,pixelHeight);
 
-        	  }
+        //start from here
+
+        if (isFreeHand) {
+            layoutCtx.beginPath();
+
+            if (temp != (prevPixelX + "," + prevPixelY)) {
+                temp = prevPixelX + "," + prevPixelY;
+            }
+            layoutCtx.moveTo(prevPixelX, prevPixelY);
+            layoutCtx.lineTo(prevPixelX, prevPixelY);
+
+            imageDataArr[prevPixelX / 10][prevPixelY / 10] = true;
+
+            if (prevPixelX != endPixelX && prevPixelY != endPixelY) {
+
+                if (temp != (endPixelX + "," + prevPixelY)) {
+                    list[count++] = temp;
+                    temp = endPixelX + "," + prevPixelY;
+
+                }
+                layoutCtx.lineTo(endPixelX, prevPixelY);
+                imageDataArr[endPixelX / 10][prevPixelY / 10] = true;
+            }
+            ;
+
+            if (temp != (endPixelX + "," + endPixelY)) {
+                list[count++] = temp;
+                temp = endPixelX + "," + endPixelY;
+            }
+            layoutCtx.lineTo(endPixelX, endPixelY);
+            imageDataArr[endPixelX / 10][endPixelY / 10] = true;
+
+            layoutCtx.strokeStyle = "rgba(255,0,0,255)";
+            layoutCtx.stroke();
+
+            prevPixelX = endPixelX;
+            prevPixelY = endPixelY;
+        }
+
+    }
+
+    document.onmouseup = function () {
+        document.onmousemove = null
+        startPixelX = Math.floor(startX / 10) * 10;
+        startPixelY = Math.floor(startY / 10) * 10;
+        endPixelX = Math.floor(endX / 10) * 10;
+        endPixelY = Math.floor(endY / 10) * 10;
+
+        pixelWidth = startPixelX - endPixelX;
+        pixelHeight = startPixelY - endPixelY;
+
+        if (pixelHeight < 0)pixelHeight = -pixelHeight;
+        if (pixelWidth < 0)pixelWidth = -pixelWidth;
+
+
+        if (mousemove) {
+            layoutCtx.strokeStyle = "rgba(255,0,0,255)";
+            layoutCtx.lineWidth = 1;
+            layoutCtx.strokeRect(startPixelX, startPixelY, pixelWidth, pixelHeight);
+        }
+        //     pixelCtx.clearRect(startPixelX,startPixelY,pixelWidth,pixelHeight);
+
+    }
 }
 
-function colourChange(){
-    for(var i=0; i<100;i++){
-        for(var j=0; j<100;j++){
-            console.log(imageDataArr[i][j]+" and "+imageDataArr[i+1][j+1]);
-            if(imageDataArr[i][j]&&imageDataArr[i+1][j+1]){
-                imageArr[i][j] = true;
+function colourChange() {
+    var style = document.getElementsByClassName('colpick_new_color')[0].style.backgroundColor;
+
+    if (isFreeHand) {
+        pixelCtx.beginPath();
+        for (var i = 0; i < list.length; i++) {
+            var tempArr = [];
+            tempArr = list[i].split(",");
+            if (i == 0) {
+                pixelCtx.moveTo(tempArr[0], tempArr[1]);
+            }
+            pixelCtx.lineTo(tempArr[0], tempArr[1]);
+        }
+        pixelCtx.closePath();
+        pixelCtx.fillStyle = style;
+        pixelCtx.fill();
+
+        for (var i = 0; i < 1000; i += 10) {
+            for (var j = 0; j < 1000; j += 10) {
+                pixelCtx.strokeStyle = "0,0,0,255";
+                pixelCtx.lineWidth = 0.01;
+                pixelCtx.strokeRect(i, j, 10, 10);
+            }
+        }
+
+    }
+//    var tempAr=[[20,20],[30,20],[40,20],[50,20],[50,10],[60,10],[60,20],[70,20],[70,30],[70,40],[60,40],[50,40],[40,40],[30,40],[20,40]];
+//    layoutCtx.beginPath();
+//    layoutCtx.moveTo(20, 20);
+//    for(var i=0;i<15;i++){
+//
+//            layoutCtx.lineTo(tempAr[i][0], tempAr[i][1]);
+//
+//    }
+//    layoutCtx.closePath();
+//    layoutCtx.stroke();
+//    layoutCtx.fillStyle = style;
+//    layoutCtx.fill();
+
+    // pixelCtx.fillRect(startPixelX,startPixelY,pixelWidth,pixelHeight);
+
+    //*************************************************************************
+    else if (!isFreeHand) {
+        for (var i = startPixelX; i < startPixelX + pixelWidth; i += 10) {
+            for (var j = startPixelY; j < startPixelY + pixelHeight; j += 10) {
+                pixelCtx.clearRect(i, j, 10, 10);
+                pixelCtx.fillStyle = style;
+                pixelCtx.lineWidth = 0.2;
+                pixelCtx.strokeRect(i, j, 10, 10);
+                pixelCtx.fillRect(i, j, 10, 10);
             }
         }
     }
-var style = document.getElementsByClassName('colpick_new_color')[0].style.backgroundColor;
-   // pixelCtx.fillRect(startPixelX,startPixelY,pixelWidth,pixelHeight);
-
-  //*************************************************************************
-//    for (var i = startPixelX; i < startPixelX+pixelWidth; i += 10) {
-//        for (var j = startPixelY; j < startPixelY+pixelHeight; j += 10) {
-//            pixelCtx.clearRect(i,j,10,10);
-//            pixelCtx.fillStyle = style;
-//            pixelCtx.lineWidth = 0.2;
-//            pixelCtx.strokeRect(i,j,10,10);
-//            pixelCtx.fillRect(i,j,10,10);
-//        }
-//    }
     //**********************************************************************
 
 
-    for(var i=0; i<100;i++){
-        for(var j=0;j<100;j++){
-         //   console.log(imageArr[i][j]);
-            if(imageArr[i][j]){
-         //       console.log('got here');
-                pixelCtx.clearRect(i*10,j*10,10,10);
+    for (var i = 0; i < 100; i++) {
+        for (var j = 0; j < 100; j++) {
+            if (imageArr[i][j]) {
+                pixelCtx.clearRect(i * 10, j * 10, 10, 10);
                 pixelCtx.fillStyle = style;
                 pixelCtx.lineWidth = 0.2;
-                pixelCtx.strokeRect(i*10,j*10,10,10);
-                pixelCtx.fillRect(i*10,j*10,10,10);
+                pixelCtx.strokeRect(i * 10, j * 10, 10, 10);
+                pixelCtx.fillRect(i * 10, j * 10, 10, 10);
             }
         }
     }
 }
 
-function pixelDel(){
-    pixelCtx.clearRect(startPixelX, startPixelY, startPixelX+pixelWidth,1);
+function pixelDel() {
+    pixelCtx.clearRect(startPixelX, startPixelY, startPixelX + pixelWidth, 1);
 }
 
 function changeContent(el) {
@@ -273,32 +314,34 @@ function clear() {
 
 
 //pixelating algo function
-function getColourValues(){
+function getColourValues() {
 
-    var pixelWidth  = canvas.width / 100;
+    var pixelWidth = canvas.width / 100;
     var pixelHeight = canvas.height / 100;
 
     var height = canvas.height;
     var width = canvas.width;
 
     var count = 0;
-    for (var i = pixelHeight/2; i < height; i += pixelHeight) {
-        for (var j = pixelWidth/2; j < width; j += pixelWidth) {
+
+    //colour mapping logic for roundup the image colour values with available yarn colours.
+    for (var i = pixelHeight / 2; i < height; i += pixelHeight) {
+        for (var j = pixelWidth / 2; j < width; j += pixelWidth) {
 
             var imgData = ctx.getImageData(i, j, 1, 1);
             var data = imgData.data;
-            var minRGB=10000000;
+            var minRGB = 10000000;
 
-            for (var k=0;k<rowCount;k++){
-                var rDist = rArr[k]*rArr[k]-data[0]*data[0];
-                var gDist = sArr[k]*sArr[k]-data[1]*data[1];
-                var bDist = lArr[k]*lArr[k]-data[2]*data[2];
-                if(rDist<0)rDist = -rDist;
-                if(gDist<0)gDist = -gDist;
-                if(bDist<0)bDist = -bDist;
+            for (var k = 0; k < rowCount; k++) {
+                var rDist = rArr[k] * rArr[k] - data[0] * data[0];
+                var gDist = sArr[k] * sArr[k] - data[1] * data[1];
+                var bDist = lArr[k] * lArr[k] - data[2] * data[2];
+                if (rDist < 0)rDist = -rDist;
+                if (gDist < 0)gDist = -gDist;
+                if (bDist < 0)bDist = -bDist;
 
-                if((rDist+gDist+bDist)<minRGB){
-                    minRGB = rDist+gDist+bDist;
+                if ((rDist + gDist + bDist) < minRGB) {
+                    minRGB = rDist + gDist + bDist;
                     rdArr[count] = rArr[k];
                     gArr[count] = sArr[k];
                     bArr[count] = lArr[k];
@@ -312,18 +355,31 @@ function getColourValues(){
     var pixelDistX = 10;
     var pixelDistY = 10;
 
-    pixelCtx.clearRect(0,0,pixelCanvas.width,pixelCanvas.height);
+    //clearing the canvas before draw
+    pixelCtx.clearRect(0, 0, pixelCanvas.width, pixelCanvas.height);
 
+    //redrawing the image data in the canvas to get pixelated pattern
     for (var i = 0; i < pixelCanvas.width; i += pixelDistX) {
         for (var j = 0; j < pixelCanvas.height; j += pixelDistY) {
-            pixelCtx.fillStyle ='rgba(' +
+            pixelCtx.fillStyle = 'rgba(' +
                 rdArr[pixelCount] + ',' + gArr[pixelCount] + ',' +
-                bArr[pixelCount]+',255' +
+                bArr[pixelCount] + ',255' +
                 ')';
-            pixelCtx.lineWidth = 0.2;
-            pixelCtx.strokeRect(i,j,pixelDistX,pixelDistY);
-            pixelCtx.fillRect(i,j,pixelDistX,pixelDistY);
+
+            pixelCtx.lineWidth = 0.01;
+            pixelCtx.strokeRect(i, j, pixelDistX, pixelDistY);
+            pixelCtx.fillRect(i, j, pixelDistX, pixelDistY);
             pixelCount++;
         }
+    }
+
+}
+
+
+function selectTool(s) {
+    if (s) {
+        isFreeHand = true;
+    } else {
+        isFreeHand = false;
     }
 }
