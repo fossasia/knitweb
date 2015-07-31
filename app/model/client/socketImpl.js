6e42cdf5af7b06335bcc16a0ad8c1c89f8690cc4
sameera <sameera910409@gmail.com>
2015-08-01 01:31:22 +0530

    socket client method for sending image data

--- app/model/client/socketImpl.js
+++ app/model/client/socketImpl.js
@@ -1,38 +1,48 @@
 function WebSocketTest()
 {
-    if ("WebSocket" in window)
-    {
+    if ("WebSocket" in window) {
         alert("WebSocket is supported by your Browser!");
 
-        var ws = new WebSocket("ws://localhost:8888/getProgess",['json']);
+        var connection = new WebSocket("ws://localhost:8888/getProgess",['json']);
 
-        ws.onopen = function()
-        {
+        connection.onopen = function() {
             // Web Socket is connected, send data using send()
             ws.send("Message to send");
             console.log("Message sent");
         };
 
-        ws.onmessage = function (evt)
-        {
+        connection.onmessage = function (evt) {
             var received_msg = evt.data;
             console.log(received_msg);
         };
 
-        ws.onerror = function(err){
+        connection.onerror = function(err){
             console.log(err);
         }
 
-        ws.onclose = function()
-        {
+        connection.onclose = function() {
             // websocket is closed.
             console.log("connection closed")
         };
     }
 
-    else
-    {
+    else {
         // The browser doesn't support WebSocket
         alert("WebSocket NOT supported by your Browser!");
     }
+
+    // Sending String
+    connection.send('your message');
+
+// Sending canvas ImageData as ArrayBuffer
+    var img = pixelCtx.getImageData(0, 0, 1000, 1000);
+    var binary = new Uint8Array(img.data.length);
+    for (var i = 0; i < img.data.length; i++) {
+        binary[i] = img.data[i];
+    }
+    connection.send(binary.buffer);
+
+// Sending file as Blob
+    var file = document.querySelector('input[type="file"]').files[0];
+    connection.send(file);
 }
